a7b7e98ca34af3aa406abf888c1d22a0694b3649
point of Studio <127053749+pointofStudio@users.noreply.github.com>
2025-03-03 23:42:42 +0100

    Update BoardConfig.mk

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -133,7 +133,7 @@ BOARD_ROOT_EXTRA_FOLDERS := firmware persist
 SELINUX_IGNORE_NEVERALLOWS := true
 SELINUX_IGNORE_NEVERALLOWS_ON_USER := true
 BOARD_SEPOLICY_DIRS += device/huawei/angler/sepolicy/vendor
-BOARD_PLAT_PRIVATE_SEPOLICY_DIR += device/huawei/angler/sepolicy/private
+SYSTEM_EXT_PRIVATE_SEPOLICY_DIRS += device/huawei/angler/sepolicy/private
 
 TARGET_USES_AOSP := true
 
